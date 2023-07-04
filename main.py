ebfdf87994f3d41b454534fe7f4b25c20affaf14
VivianT33 <viviantai5441@gmail.com>
2023-07-05 04:05:40 +0800

    third round2

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def handle_message(event):
 
     k =1
     while k !=0:
-        eval (message = TextSendMessage(text='Hello,what can I help you? \n Please enter the number of the list below \n 1.food \n 2.transportation \n 3.cloth \n 4.living \n 5.exit \n '))
+        message = eval (TextSendMessage(text='Hello,what can I help you? \n Please enter the number of the list below \n 1.food \n 2.transportation \n 3.cloth \n 4.living \n 5.exit \n '))
         line_bot_api.reply_message(event.reply_token, message)
         k == message 
         if k==1 :
